63103633dbe35fa096d3d7ea3749c19734a906ea
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-09-10 20:24:13 +0200

    Navbar and Product card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Another try</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
